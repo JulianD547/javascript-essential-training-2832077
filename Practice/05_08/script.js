e7f964e4a0c6518cd67eed81364db257eaee95bc
Julian DeLaRosa <juliandelarosa547@gmail.com>
2024-10-02 16:55:30 -0500

    Completed section 4 and almost 5
    continuing lessons on Javascript, almost done with seciont 5

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,10 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+//Messing around with Javascript stuff
+
+/* 
+    request, change, specify style property, etc
+
+*/
